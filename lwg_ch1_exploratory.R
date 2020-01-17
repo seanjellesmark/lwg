16f454d7be1efa8d6bef7755861e74a98209e267
seanjellesmark <59930036+seanjellesmark@users.noreply.github.com>
2020-01-17 10:27:56 +0000

    Show Gonzalo

--- lwg_ch1_exploratory.R
+++ lwg_ch1_exploratory.R
@@ -1,12 +1,15 @@
-test<-bbs_grass_species%>%pivot_longer(cols = S1:S10,
+test<-bbs_grass_species%>%pivot_longer (cols = S1:S10,
                                  names_to = "Transect",
-                                 values_to = "Count",
-                                 names_prefix = gsub("[a-zA-Z ]", "")
+                                 values_to = "Count")
+test$Transect<-as.numeric(gsub("S", "", test$Transect))
+                                 
+                                 
+                                 
 
                                  
                                  
 test1<-test%>%
   group_by(Gridref, year, EarlyLate, species)%>%
-  summarise(counts = sum(Count, na.rm = TRUE)
+  summarise(max_count = sum(Count, na.rm = TRUE)
             
                                                                       
