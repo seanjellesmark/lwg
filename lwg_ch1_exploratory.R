1fc9170656582ccc88f51429aa9b560464c4ab58
seanjellesmark <59930036+seanjellesmark@users.noreply.github.com>
2020-01-17 16:15:40 +0000

    Changed when the habitat filtering happens

--- lwg_ch1_exploratory.R
+++ lwg_ch1_exploratory.R
@@ -3,18 +3,19 @@ test<-bbs_species%>%pivot_longer (cols = S1:S10,
                                  values_to = "Count")
 test$Transect<-as.numeric(gsub("S", "", test$Transect))
 
-test1<-left_join(bbs_habitat_all, test, by = c("year","Gridref","Transect"))                                 
-test2<-test2%>%
+bbs_wet_grasslands<-bbs_habitat_all%>%
   filter(PLevel1=="C" & PLevel2 == 6 | SLevel1=="C" & SLevel2 == 6)
 
-test3<-test3%>%
+test11<-left_join(bbs_wet_grasslands, test, by = c("year","Gridref","Transect"))                                 
+
+test22<-test11%>%
   filter(is.na(RSPBOverlap))
                                  
-test4<-test3%>%
+test44<-test22%>%
   group_by(Gridref, year, EarlyLate, species)%>%
   summarise(EandL_max_count = sum(Count, na.rm = TRUE))
 
-test5<-test4%>%group_by(Gridref, year, species)%>%
+test55<-test44%>%group_by(Gridref, year, species)%>%
   summarise(max_count = max(EandL_max_count))
 
 test_bbs_lapwing<-test5%>%
@@ -24,7 +25,8 @@ test_lapwing_trim<-trim(max_count~Gridref+year, data=test_bbs_lapwing, model=3, 
 test_lapwing_index<-index(test_lapwing_trim)
 plot(test_lapwing_index)
 test_results_lapwing<-results(test_lapwing_trim)
-
+imputed_test_lapwing<-imputed(test_results_lapwing)
+pbserved_test_lapwing<-observed(test_results_lapwing)
 # for Redshank
 test_bbs_redshank<-test5%>%
   filter(species =="RK" | is.na(species))
@@ -33,3 +35,7 @@ test_redshank_trim<-trim(max_count~Gridref+year, data=test_bbs_redshank, model=3
 test_redshank_index<-index(test_redshank_trim)
 plot(test_redshank_index)
 test_results_redshank<-results(test_redshank_trim)
+
+# difference between the grids
+setdiff(lapwing_trim[["site_id"]], test_lapwing_trim[["site_id"]])
+setdiff(test_lapwing_trim[["site_id"]], lapwing_trim[["site_id"]])
